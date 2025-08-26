8108a8d03d93c2b63ab3adeb0cca83b88bd7fd73
Alexander Korolev <alexander.korolev.germany@gmail.com>
2025-08-26 09:44:51 +0200

    upgrade to latest static-files 0.3.1 and not yet released actix-web-static-files

--- frontend/build.rs
+++ frontend/build.rs
@@ -1,8 +1,12 @@
-use static_files::NpmBuild;
+use static_files::{NodeModulesStrategy, NpmBuild};
 
 fn main() {
+    unsafe {
+        std::env::set_var("NODE_OPTIONS", "--openssl-legacy-provider");
+    }
     NpmBuild::new("./web")
-        .target("./web/dist/angular-router-sample")
+        .node_modules_strategy(NodeModulesStrategy::MoveToOutDir)
+        .target("node_modules/dist/angular-router-sample")
         .install()
         .unwrap()
         .run("build")
