21cdc830ce910a1935ca999af01b8160369bc5f0
Alexander Korolev <alexander.korolev.germany@gmail.com>
2025-09-07 09:34:02 +0200

    reduce cognitive load

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,24 +1,15 @@
 use actix_web::{middleware::Logger, App, HttpServer};
-#[cfg(feature = "ui")]
-use actix_web_static_files;
-
-#[cfg(feature = "ui")]
+use actix_web_static_files::ResourceFiles;
 use angular_example_frontend::generate;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
     HttpServer::new(move || {
-        let mut app = App::new().wrap(Logger::default());
-        #[cfg(feature = "ui")]
-        {
-            let generated = generate();
-            app = app.service(
-                actix_web_static_files::ResourceFiles::new("/", generated)
-                    .resolve_not_found_to_root(),
-            );
-        }
-        app
+        let generated = generate();
+        App::new()
+            .wrap(Logger::default())
+            .service(ResourceFiles::new("/", generated).resolve_not_found_to_root())
     })
     .bind("127.0.0.1:8080")?
     .run()
